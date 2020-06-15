3a4a40bf7ec0d6506e110c8a1a4489d007b8d0e1
HughG-50 <hugh.greethead@gmail.com>
2020-06-15 16:14:21 +1000

    Forex rates API parsing

--- forexApi/forex.js
+++ forexApi/forex.js
@@ -1,12 +1,19 @@
 //Simple API for displaying the exchange rate between the base pair of USD or EUR against other currencies
 
-const publicEndpoint = 'https://open.exchangerate-api.com/v6/latest'
-const freeEndpoint = `https://v6.exchangerate-api.com/v6/ea62ff5ffc1dfea118f56539/latest/USD`
+// const publicEndpoint = 'https://open.exchangerate-api.com/v6/latest'
+const freeEndPoint = 'https://v6.exchangerate-api.com/v6/ea62ff5ffc1dfea118f56539/latest/USD'
 
 function getExchangeRate() {
-    fetch(freeEndpoint)
-    .then(response => console.log(response.data))
+    fetch(freeEndPoint)
+    .then(response => response.json())
+    .then(data => {
+        //console.log(data)
+        console.log(data.base_code)
+        console.log(data.conversion_rates)
+    })
     .catch(err => console.log(err));
 }
 
+
+//better to use async await than chaining .thens in this case - i.e. tell JS to wait until async code is finished
 getExchangeRate()
